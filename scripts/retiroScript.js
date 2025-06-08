aaa5612b9a8eded572ed67b424908a0bba0901c5
DavidA <dserrudocaina@gmail.com>
2025-06-08 12:15:21 -0500

    feat: agregar pantalla de retiro con HTML, CSS y JS

---
+++ scripts/retiroScript.js
@@ -0,0 +1,12 @@
+function activarBoton(elemento1,elemento2,elemento3,elemento4, clase) {
+    const boton1 = document.getElementById(elemento1);
+    const boton2 = document.getElementById(elemento2);
+    const form1 = document.getElementById(elemento3);
+    const form2 = document.getElementById(elemento4);
+
+    boton1.classList.add(clase);
+    form1.classList.add(clase);
+    boton2.classList.remove(clase);
+    form2.classList.remove(clase);
+
+}
