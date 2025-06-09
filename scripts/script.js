3aac9d36730bcf1b554a7fb988ffec30c8ac3c8a
DavidA <dserrudocaina@gmail.com>
2025-06-08 23:10:02 -0500

    feat: crear pantalla de resumen financiero

--- scripts/script.js
+++ scripts/script.js
@@ -22,6 +22,88 @@ function mostrarNotificacion(event, mesage){
         notification.classList.remove("mostrar");
         window.location.href = "miBalance.html";
     }, 1200);
+}
+
+const ctx = document.getElementById('graficoIngresosGastos');
 
 
+//Tabla Ingresos vs Gastos
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado','Domingo'],
+        datasets: [{
+            label: 'Ingresos',
+            data: [70, 90, 40, 80, 65, 85, 110],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'white',
+            borderWidth: 1,
+
+        }, {
+            label: 'Gastos',
+            data: [60, 55, 110, 35, 55, 70, 60],
+            backgroundColor: '#d4c44f',
+            borderColor: 'white',
+            borderWidth: 1,
+
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    color: 'white',
+                    font: {
+                        size: 14
+                    }
+                }
+            },
+            title: {
+                display: false,
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {color: 'white',font: { size: 14,family: 'Poppins'}},
+                grid: {
+                    color: 'rgba(255, 255, 255, 0.2)'
+                }
+            },
+            x: {
+                ticks: { color: 'white',  font: {size: 14,family:'Poppins'}},
+                grid: {
+                    color: 'rgba(255, 255, 255, 0.2)'
+                }
+            }
+        }
+
+
+
+    }
+});
+
+
+//Tabla Gastos del Mes
+
+const grid = document.getElementById('grid-dias');
+const totalDias = 196;
+const nivelesColor = [
+  '#ffffff',     
+  '#c4b885',     
+  '#b0a14d',     
+  '#88762c',     
+  '#5f521c'      
+];
+
+for(let i=0; i< totalDias; i++){
+    const dia = document.createElement('div');
+    dia.classList.add('dia');
+
+    const nivel = Math.floor(Math.random()*nivelesColor.length);
+    dia.style.backgroundColor = nivelesColor[nivel];
+
+    grid.appendChild(dia);
 }
